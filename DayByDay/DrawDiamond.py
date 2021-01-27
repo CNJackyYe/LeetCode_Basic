c3b7e5e21ed700d554b1637d1b372ef26fc21c6b
Jacky Ye <yeqw321@163.com>
2021-01-27 17:17:52 +0800

    添加菱形题目

--- DayByDay/DrawDiamond.py
+++ DayByDay/DrawDiamond.py
@@ -1,16 +1,36 @@
 # 画菱形 2021/1/28
-# 题目 打印出如下图案（菱形）:
-#       *
-#       ***
-#       *****
-#       *******
-#       *****
-#       ***
-#       *
+# 题目 输入一个整数num，输出对应的菱形
+# 如输入num=3
+#   *
+#  ***
+# *****
+#  ***
+#   *
+# 如输入num=4
+#    *
+#   ***
+#  *****
+# *******
+#  *****
+#   ***
+#    *
+
 
 
 class Solution:
-    def DrawDiamond(self, nums: int):
-        a = nums*2-1
+    def DrawDiamond(self, num: int):
+        self.DrawSon(1, (num * 2 - 1))
 
-        pass
+    def DrawSon(self, num: int, total: int):
+        if (num <= (total + 1) / 2):
+            a = int((total + 1) / 2 - num) * ' ' + (num * 2 - 1) * '*'
+            print(a)
+        else:
+            a = int(num - (total + 1) / 2) * ' ' + ((total + 1 - num) * 2 - 1) * '*'
+            print(a)
+        if (num <= total):
+            self.DrawSon(num + 1, total)
+
+
+su = Solution()
+su.DrawDiamond(4)
