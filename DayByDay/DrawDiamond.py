e442b6b19f53268be06665c30faa86bee7ab2838
Jacky Ye <yeqw321@163.com>
2021-01-27 12:46:11 +0800

    添加画菱形题目

---
+++ DayByDay/DrawDiamond.py
@@ -0,0 +1,16 @@
+# 画菱形 2021/1/28
+# 题目 打印出如下图案（菱形）:
+#       *
+#       ***
+#       *****
+#       *******
+#       *****
+#       ***
+#       *
+
+
+class Solution:
+    def DrawDiamond(self, nums: int):
+        a = nums*2-1
+
+        pass
